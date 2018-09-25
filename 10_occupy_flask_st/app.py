6e9aa6303fd50959be7b0033ce18ee07075561d6
tina <tinaw425@gmail.com>
2018-09-24 20:13:45 -0400

    added comments to work 10

--- 10_occupy_flask_st/app.py
+++ 10_occupy_flask_st/app.py
@@ -4,12 +4,15 @@
 # 2018-09-24
 
 from flask import Flask, render_template
-app = Flask(__name__)
+app = Flask(__name__) #create instance of class Flask
+
 
 import csv
 import random
 occDict = {}
 
+# takes in a csv file and adds each row into the dictionary,
+# with its "Job Class" as the key and the "Percentage" as the value
 def handle_csv():
     data = open("data/occupations.csv", 'rt')
     occCsvReader = csv.reader( data)
@@ -19,17 +22,20 @@ def handle_csv():
             occDict[row[0]] = float(row[1])
 handle_csv()
 
+# maintains the proportion between the occupation percentages by adding them to a list of size 998
+# selects and returns a random occupation
 def random_occupation():
     occupation = []
     for key in occDict:
         occupation = occupation + [key] * int(occDict[key] * 10)
     return random.choice(occupation)
 
-@app.route("/")
+@app.route("/") #assign fxn to route
 def home():
-    return "<a href=/occupations>Occupation Table</a>"
+    return "<a href=/occupations>Occupation Table</a>" # provides a link to the occupations route
 
-@app.route("/occupations")
+# returns the template and values that will replace the Jinja2 in the template
+@app.route("/occupations") #assign fxn to route
 def table():
     return render_template("occupations.html",
                             title = "Occupations Table",
