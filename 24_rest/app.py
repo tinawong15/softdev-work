aac24f9ef474380cfc9c455ca356bd5953d910aa
tina <tinaw425@gmail.com>
2018-11-13 19:21:37 -0500

    less confused, finished work 24

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -10,11 +10,13 @@ app = Flask(__name__) #create instance of class Flask
 
 @app.route("/") #assign fxn to route
 def index():
-    u = urllib.request.urlopen("https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2014-02-01&cloud_score=True&api_key=DEMO_KEY")
-    response = u.read()
-    data = json.loads(response)
+    u = urllib.request.urlopen("https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2014-02-01&cloud_score=True&api_key=M28pE64z543Z5HEA9ommZK9RxgQRXGEs5szzuyJv")
+    response = u.read() # reads url
+    # print(response)
+    data = json.loads(response) # turns a JSON object string into a Python dictionary
+    # print(data)
     return render_template("index.html", pic=data['url'])
-    
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
