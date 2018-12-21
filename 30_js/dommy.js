b5525c9829033c9e4ccf81e9b6246783ddaf7092
tina <tinaw425@gmail.com>
2018-12-21 13:05:33 -0500

    added comments

--- 30_js/dommy.js
+++ 30_js/dommy.js
@@ -3,6 +3,7 @@
 // K30 -- Sequential Progression III: Season of the Witch
 // 2018-12-20
 
+// add a new list element to thelist with WORD displayed
 var addElement = function() {
   var ol = document.getElementById("thelist");
   var newElem = document.createElement("li");
@@ -10,6 +11,7 @@ var addElement = function() {
   ol.appendChild(newElem);
 }
 
+// fib function
 var fibonacci = function(n) {
   if(n < 2) {
     return n; // base case
@@ -19,6 +21,7 @@ var fibonacci = function(n) {
   }
 }
 
+// add the next Fib number as the next list element for the Fib list
 var countFib = 0;
 var addFib = function() {
   var ol = document.getElementById("fiblist");
@@ -29,18 +32,28 @@ var addFib = function() {
 }
 
 var button = document.getElementById('b');
+
+// when the button is clicked, call the addElement function
 button.addEventListener("click", addElement);
 
 var fib = document.getElementById('fb');
+
+// when the fib button is clicked, call the addFib function
 fib.addEventListener("click", addFib);
 
 var ol = document.getElementById("thelist");
+
+// when the mouse is over a list element, change the heading to the content of the list element
 ol.addEventListener('mouseover', function(e) {
   document.getElementById('h').innerHTML = e.target.innerHTML;
 });
+
+// when the mouse is not on a list element, revert the heading to 'Hello World!'
 ol.addEventListener('mouseout', function(e) {
   document.getElementById('h').innerHTML = "Hello World!";
 });
+
+// when the item is clicked, remove it
 ol.addEventListener('click', function(e) {
   e.target.remove();
 });
